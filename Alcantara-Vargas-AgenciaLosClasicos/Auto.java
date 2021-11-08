8901713b8b2ccf4fd71252c7a21810c1d06c5f30
Eduardo Vargas Pérez <eduardovar1090@ciencias.unam.mx>
2021-11-08 10:19:47 -0600

    Error en doc

--- Alcantara-Vargas-AgenciaLosClasicos/Auto.java
+++ Alcantara-Vargas-AgenciaLosClasicos/Auto.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Clase que representa a un automóvil
  * @author Isaac Alcantara y Eduardo Vargas
  * @version 1.0
@@ -20,7 +20,7 @@ public class Auto{
 
     //Métodos
     //Métodos Constructores
-    /*
+    /**
      * Constructor sin parametros, crea un automóvil de la marca Toyota, 
      * modelo Prius, del año 2021 y color rojo
      * 
@@ -33,7 +33,7 @@ public class Auto{
     }
     
     //Métodos Observadores
-    /*
+    /**
      * Método que devuelve la marca del automóvil
      *
      * @return La marca del auto
@@ -42,7 +42,7 @@ public class Auto{
 	return this.marca;
     }
 
-    /*
+    /**
      * Método que devuelve el modelo del auto (nombre)
      *
      * @return El nombre del modelo del auto
@@ -51,7 +51,7 @@ public class Auto{
 	return this.modelo;
     }
 
-    /*
+    /**
      * Método que devuelve el año de fabricación del automóvil
      *
      * @return El año de fabricación del auto.
@@ -60,7 +60,7 @@ public class Auto{
 	return this.ano;
     }
 
-    /*
+    /**
      * Método que devuelve el precio del automóvil
      *
      * @return El precio del auto
@@ -69,7 +69,7 @@ public class Auto{
 	return this.precio;
     }
 
-    /*
+    /**
      * Método que devuelve el color del automóvil
      *
      * @return El color del auto.
