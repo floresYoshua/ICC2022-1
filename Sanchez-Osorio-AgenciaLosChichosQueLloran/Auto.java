5c69c0e98ff130825eb6a0609e3abc45849a3419
ethand <ethandamian@ciencias.unam.mx>
2021-11-11 19:46:00 -0600

    Modifique Auto.java y agregue el main Agencia.java

--- Sanchez-Osorio-AgenciaLosChichosQueLloran/Auto.java
+++ Sanchez-Osorio-AgenciaLosChichosQueLloran/Auto.java
@@ -12,28 +12,83 @@ public class Auto{
     private String color;
 
     //el modelo del auto
-    private int noPuertas;
+    private String noPuertas;
 
     //El año del modelo
     private String año;
 
+    //El auto es electrico o no
+    private boolean electrico;
+
+    //El precio del auto
+    private String precio;
+
     //CONSTRUCTORES
 
     /**
-     * Constructor que recibe cuatro paramteros
+     * Constructor que recibe cinco paramteros
      * @param modelo El modelo del auto 
      * @param color El color del auto
      * @param noPuertas El numero de puertas del auto 
      * @param año EL año del auto
+     * @param electrico Si el auto es electrico o no 
      */
 
-    public Auto(String modelo, String color, int noPuertas, String año){
+    public Auto(String modelo, String color, String noPuertas, String año, boolean electrico, String precio){
 	this.modelo = modelo;
 	this.color = color;
 	this.noPuertas = noPuertas;
-	this.año = año;
+	this.año = año; 
+	this.electrico = electrico;
+	this.precio = precio;
     } 
 
+    /**
+     * Constructor que recibe el color del auto -- El auto es un aveo, con color a elegir 
+     * @param color EL color del auto
+     */       
+
+    public Auto(String color){
+	this.modelo = "Chevrolet Aveo";
+	this.color = color;
+	this.noPuertas = "4";
+	this.año = "2022";
+	this.electrico = false;
+	this.precio = "229,200";
+	
+    }
+
+    /**
+     * Constructor sin parametros-- representa a un Chevrolet Groove 2022
+     */
+
+    public Auto(){
+	this.modelo = "Chevrolet Groove";
+	this.color = "rojo";
+	this.noPuertas = "4";
+	this.año = "2022";
+	this.electrico = false;
+	this.precio = "332,900";
+
+    }
+
+    /**
+     * Constructor que recibe dos parametro y que representa a un Chevrolet Volt EUV 2020
+     * @param electrico Si el auto es electrico o no.
+     * @param precio El nuevo precio del auto
+     */
+    public Auto(boolean electrico, String precio){
+	this.modelo = "Chevrolet Volt EUV";
+	this.color = "blanco";
+	this.noPuertas = "4";
+	this.año = "2020";
+	this.electrico = electrico;
+	this.precio = precio;
+	    
+
+    }
+
+    
     //METODOS
     
     //GETTERS
@@ -65,7 +120,7 @@ public class Auto{
      * @return El numero de puertas del auto 
      */
 
-    public int getNoPuertas(){
+    public String getNoPuertas(){
 	return this.noPuertas; 
 
     }
@@ -80,6 +135,28 @@ public class Auto{
 	return this.año; 
 
     }
+
+        /**
+     * Metodo que devuelve si el auto es electrico o no  
+     * @return Si el auto es electrico o no  
+     */
+
+    public boolean getElectrico(){
+	return this.electrico; 
+
+    }
+
+    
+        /**
+     * Metodo que devuelve el precio del auto  
+     * @return El precio del auto 
+     */
+
+    public String getPrecio(){
+	return this.precio; 
+
+    }
+
 
     //SETTERS
     /**
@@ -109,7 +186,7 @@ public class Auto{
      * @param noPuertas El nuevo numero de puertas del auto  
      */
 
-    public void setNoPuertas(int noPuertas){
+    public void setNoPuertas(String noPuertas){
 	this.noPuertas = noPuertas;
     }
 
@@ -123,6 +200,26 @@ public class Auto{
 	this.año = año;
     }
 
+    /**
+     * Metodo que cambia si el auto es electrico o no 
+     * @param modelo El nuevo valor de si es electrico o no   
+     */
+
+    public void setElectrico(boolean electrico){
+	this.electrico = electrico;
+    }
+
+    /**
+     * Metodo que cambia el precio del auto
+     * @param precio El nuevo precio del auto  
+     */
+
+    public void setPrecio(String precio){
+	this.precio = precio;
+    }
+    
+
+
     //METODOS EPECIALES
 
     /**
@@ -131,7 +228,13 @@ public class Auto{
      */
 
     public String toString(){
-	return "Modelo : " + this.modelo +"\n" + "Color : " + this.color + "\n" + "No. de puertas : " + this.noPuertas + "\n" + "Año: " + this.año;
+	
+	return "Modelo : " + this.modelo +"\n"
+	    + "Color : " + this.color + "\n" +
+	    "No. de puertas : " + this.noPuertas + "\n"
+	    + "Año: " + this.año + "\n"
+	    + "Electrico: " + this.electrico + "\n"
+	    + "Precio: $" + this.precio;
 	
     }
 
