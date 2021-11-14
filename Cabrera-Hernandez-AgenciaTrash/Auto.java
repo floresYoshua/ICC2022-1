ea4aace767c90acab38849e2a986ad0d7eb49c76
DhamarCabrera <92839629+DhamarCabrera@users.noreply.github.com>
2021-11-14 02:03:07 -0600

    Agregué metodo de acceso.

--- Cabrera-Hernandez-AgenciaTrash/Auto.java
+++ Cabrera-Hernandez-AgenciaTrash/Auto.java
@@ -1,4 +1,4 @@
-/**
+**
 * Clase que define los atributos y métodos de un Auto de la Agencia Trash.
 * @author : Dhamar Cabrera.
 * @author : Elizabeth Hernandez.
@@ -14,45 +14,81 @@ public class Auto {
   private int n_llantas; //cuántas llantas tiene
   private int n_puertas; //cuántas puertas tiene
   private double precio; //qué precio tiene
+  
+    /**
+     * Constructor por omisión
+     * @return : marca
+     * @return : modelo
+     * @return : tipo
+     * @return :color
+     * @return : n_llantas
+     * @return : n_puertas
+     * @return : precio
+     */
 
+    public Auto (){
+	this.marca = "Nissan";
+	this.modelo = "Versa";
+	this.tipo = "Sedan";
+	this.color = "Rojo";
+	this.n_llantas = 4;
+	this.n_puertas = 4;
+	this.precio = 276.900;
+    }
+  
+    /**
+     * Constructor con parámetros
+     * @param : marca
+     * @param : modelo
+     * @param : tipo
+     * @param :color
+     * @param : n_llantas
+     * @param : n_puertas
+     * @param : precio
+     */
+    public Auto ( String marca, String modelo, String tipo, String color, int n_llantas, int n_puertas, double precio){
+	this.marca = marca;
+	this.modelo = modelo;
+	this.tipo = tipo;
+	this.color = color;
+	this.n_llantas = n_llantas;
+	this.n_puertas = n_puertas;
+	this.precio = precio;
+    }
 
-  /**
-  * Constructor por omisión
-  * @return : marca
-  * @return : modelo
-  * @return : tipo
-  * @return :color
-  * @return : n_llantas
-  * @return : n_puertas
-  * @return : precio
-  */
-  public AutoTrash (){
-    this.marca = "Nissan";
-    this.modelo = "Versa";
-    this.tipo = "Sedan";
-    this.color = "Rojo";
-    this.n_llantas = 4;
-    this.n_puertas = 4;
-    this.precio = 276.900;
-  }
+    /**
+     * Acceso a características del auto.
+     * @return : marca
+     * @return : modelo
+     * @return : tipo
+     * @return :color
+     * @return : n_llantas
+     * @return : n_puertas
+     * @return : precio
+     */
+    public String getMarca ( String marca ){
+	return this.marca = marca;
+    }
 
-  /**
-  * Constructor con parámetros
-  * @param : marca
-  * @param : modelo
-  * @param : tipo
-  * @param :color
-  * @param : n_llantas
-  * @param : n_puertas
-  * @param : precio
-  */
-  public AutoTrash ( Strring marca, String modelo, String tipo, String color, int n_llantas, int n_puertas, double precio){
-    this.marca = marca;
-    this.modelo = modelo;
-    this.tipo = tipo;
-    this.color = color;
-    this.n_llantas = n_llantas;
-    this.n_puertas = n_puertas;
-    this.precio = precio;
-  }
+    public String getModelo ( String modelo ){
+	return this.modelo = modelo;
+    }
+
+    public String getTipo ( String tipo ){
+	return this.tipo = tipo;
+    }
+        public String getColor ( String color ){
+	return this.color = color;
+    }
+    
+    public int getLlantas ( int n_llantas ){
+	return this.n_llantas = n_llantas;
+    }
+
+    public int getPuertas ( int n_puertas ){
+	return this.n_puertas = n_puertas;
+    }
+    public double getPrecio ( double precio){
+	return this.precio = precio;
+    }
 }
