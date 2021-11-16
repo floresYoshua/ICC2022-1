5f96fabbd03b0d0557b44020805ef336d93a5371
dulcemora <dulcemora03@ciencias.unam.mx>
2021-11-15 19:53:11 -0600

    Agregue atributos de la clase comprador

---
+++ Mora-Resendiz-AgenciaMore/Comprador.java
@@ -0,0 +1,14 @@
+/**
+ * Clase que nos da los datos de un Comprador
+ * @author MORE
+ * @version 1.0
+ **/
+public class Comprador {
+
+    //Atributos
+    private String nombre;
+    private int autocompra;
+    private int añocompra;
+    private String ultcompra;
+    private String tipopago;
+    private int historial;
