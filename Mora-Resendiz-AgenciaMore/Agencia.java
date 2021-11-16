7100a38d61ea2a79d18afb6a87ae59e537083673
dulcemora <dulcemora03@ciencias.unam.mx>
2021-11-15 20:29:46 -0600

    Agregue lo de switch

--- Mora-Resendiz-AgenciaMore/Agencia.java
+++ Mora-Resendiz-AgenciaMore/Agencia.java
@@ -18,3 +18,40 @@ public class Menu{
       	    System.out.println("7. Salir");
 	    System.out.print("Introducir el numero de la accion que desea realizar: ");
 	    opcion = sc.nextInt();
+
+	    switch (opcion){
+	    case 1:
+		    System.out.println("Selecciono la opcion 1. AUTOS MÁS VENDIDOS");
+		    break;    
+		   
+	    case 2:
+		    System.out.println("Selecciono la opcion 2");
+		    //		    int opcion1;
+		    //switch(opcion1){}
+		    break;
+
+      	    case 3:
+		    System.out.println("Selecciono la opcion 3");
+		    break;
+
+	    case 4:
+		    System.out.println("Selecciono la opcion 4");
+		    break;
+
+	    case 5:
+		    System.out.println("Selecciono la opcion 5");
+		    break;
+
+	    case 6:
+		    System.out.println("Selecciono la opcion 6. CLIENTES");
+		    break;
+
+	    case 7:
+		    salida = true;
+		    break;
+		    //Para que solo este en el rango
+	    default:
+		System.out.println("Las opciones disponibles son del 1 al 6");
+	    }       
+    }
+}
