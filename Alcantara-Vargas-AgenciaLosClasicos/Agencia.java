9118a9ed5a9c50447e918d00535edbd3c229576c
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2021-11-15 20:16:02 -0600

    Añadimos cosas al archivo

--- Alcantara-Vargas-AgenciaLosClasicos/Agencia.java
+++ Alcantara-Vargas-AgenciaLosClasicos/Agencia.java
@@ -16,9 +16,17 @@ public class Agencia{
 	//Imprimir el nombre de la agencia y si podemos ayudar en algo
 	System.out.println("***AGENCIA LOS CLASICOS***" + "\n"+ "Bienvenido, ¿podemos ayudarle en algo? (ingresar el numero) \n 1. Deseo hablar con un vendedor \n 2. Solamente estoy viendo");
 
+
+	Auto ToyotaPrius = new Auto();
+	Auto KiaRent2018 = new Auto("Kia", "Forte Sedán", 2018, 4560.50, "Rojo");
+	Auto KiaRent2014 = new Auto("Kia", "Rio Sedán", 2014, 3600.80, "Azul");
+       	Auto ChevroletRent2021 = new Auto("Chevrolet", "Aveo", 2021, 5600.30, "Arena");
+	Auto ChevroletRent2010 = new Auto("Chevrolet", "Cavalier", 2010, 3350.00, "Blanco");
+	Auto ChevroletRent1990 = new Auto("Chevrolet", "Camaro", 1990, 6700.27, "Negro");
 	//Objetos de la clase Scanner para las respuestas del usuario
 	Scanner numeros = new Scanner(System.in);
 	Scanner strings = new Scanner(System.in);
+	Scanner rentanum = new Scanner(System.in);
 	
 	//Para la respuesta del usuario
 	int respuesta = numeros.nextInt();
@@ -58,7 +66,39 @@ public class Agencia{
 		break;
 	    case 2:     //En caso de querer rentar
 		System.out.println("De acuerdo " + cliente.getNombre() + " "  + cliente.getApellido() + "\nEn esta agencia solamente manejamos 3 marcas de autos para rentar: \n 1. Kia \n 2. Toyota \n 3. Chevrolet");
+		//Se solicita al usuario que elija la marca del coche que desea rentar
+		System.out.println("\n Elija la marca de coche que desea rentar seleccionando el número asociado a este");
+		//Para la respuesta del usuario
+		int respRenta = rentanum.nextInt();
+		respRenta = Math.abs(respRenta); //Tomemos el valor absoluto
+		switch(respRenta){
+
+		case 1: System.out.println("\n Actualmente contamos sólo con dos ejemplares disponibles de esta marca para rentar");
+		    
+		    System.out.println("1.Primer ejemplar \n" + KiaRent2018.toString() + " (Por día de renta) \n");
+
+		     System.out.println("2.Segundo ejemplar \n" + KiaRent2014.toString() + " (Por día de renta)");
+		     System.out.println("Elija el ejemplar que desea rentar escribiendo el número asociado a este");
+	//Para la respuesta del usuario
+		int autoRenta = rentanum.nextInt();
+		autoRenta = Math.abs(autoRenta); //Tomemos el valor absoluto
+		 double precio;
+		 int dias;
+		 dias=5;
+		if (respRenta==1){
+		     precio = KiaRent2018.getPrecio();
+		     cliente.Rentar(dias, precio);
+
+		}else if (respRenta==2){
+		    precio = KiaRent2014.getPrecio();
+		    cliente.Rentar(dias, precio);
+		} else{
+		    System.out.println("Lo siento, sólo contamos con esos modelos y usted seleccionó un modelo con el que no contamos actualmente");
+		}
+		System.out.println("Espero haber sido de ayuda, hasta luego");
 		break;
+		}
+               
 	    }	
 	}else if(respuesta == 2){
 	    System.out.println("Estamos para servirle.");
