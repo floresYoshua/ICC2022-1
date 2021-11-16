413b06c7e6062ffc87ba74d09478c96ffe540f3c
dulcemora <dulcemora03@ciencias.unam.mx>
2021-11-15 19:53:49 -0600

    Agregue constructores en la clase comprador

--- Mora-Resendiz-AgenciaMore/Comprador.java
+++ Mora-Resendiz-AgenciaMore/Comprador.java
@@ -12,3 +12,49 @@ public class Comprador {
     private String ultcompra;
     private String tipopago;
     private int historial;
+
+    /**
+     * Constructor que recibe 6 valores, 3 de tipo String y 3 de tipo int
+     * @param nombre Representa el nombre del cliente
+     * @param autocompra Representa el auto que se esta comprando
+     * @param añocompra Representa el año en el que se hace la compra(año actual)
+     * @param ultcompra Representa la fecha de la ultima compra
+     * @param tipopago Representa como se pagara, en efectivo o tarjeta
+     * @param historial Representa el numero de autos que ha comprado
+     **/
+    public Comprador(String nombre, int autocompra, int añocompra, String ultcompra, String tipopago, int historial){
+    this.nombre = nombre;
+    this.autocompra = autocompra;
+    this.añocompra = añocompra;
+    this.ultcompra = ultcompra;
+    this.tipopago = tipopago;
+    this.historial = historial;
+    }
+
+     /**
+     * Constructor que recibe 4 valores, 2 de tipo String y 2 de tipo int(en ese orden)
+     * @param nombre Representa el nombre del cliente
+     * @param ultcompra Representa la fecha de la ultima compra
+     * @param añocompra Representa el año en el que se hace la compra(año actual)
+    * @param historial Representa el numero de autos que ha comprado
+     **/
+    public Comprador(String nombre, String ultcompra, int añocompra, int historial){
+    this.nombre = nombre;
+    this.añocompra = añocompra;
+    this.ultcompra = ultcompra;
+    this.historial = historial;
+    }
+
+    /**
+     * Constructor que recibe 4 valores, 2 de tipo String y 2 de tipo int
+     * @param nombre Representa el nombre del cliente
+     * @param autocompra Representa el auto que se esta comprando
+     * @param tipopago Representa como se pagara, en efectivo o tarjeta
+     * @param historial Representa el numero de autos que ha comprado
+     **/    
+    public Comprador(String nombre, int autocompra, String tipopago, int historial){
+    this.nombre = nombre;
+    this.autocompra = autocompra;
+    this.tipopago = tipopago;
+    this.historial = historial;
+    }
